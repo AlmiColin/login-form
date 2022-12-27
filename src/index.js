1ecc9b168ae0fc40c70156af4c90d9fcf1ce3613
AlmiColin <koysheva.anastasia@yandex.ru>
2022-12-27 17:53:07 +0300

    Добавляет форму входа

--- src/index.js
+++ src/index.js
@@ -1,41 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const users = [
+  { login: "Korgi", password:"Gav" },
+  { login: "Kisa", password:"Mao" },
+];
 
 class Form extends React.Component {
+  state = {};  
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+    const user = users.find(item => item.login === login&&item.password === password);
+    this.setState({user});
+  };
+
+  handleClick = (e) => {
+    this.setState({user: false})
+  };
 
   render() {
-    return(
-      <div class = 'container'>
-        <form class = 'form'>
-          <h1 class ='form-title'>Войти:</h1>
-            <div class='form-group'>
-                  <input class='form-input' placeholder='Адрес электронной почты'/>
-                  
-            </div>
-            <div class ='form-group'>
-                  <input class='form-input' placeholder='Пароль' type='password'/>
-            </div>
-                  <button class ='button'>Войти</button>
-            <div>
-                  <a class = 'links' href='#'>Забыли пароль?</a>
-            </div>
-            <div>
-                  <a class = 'links' href='#'>Создать аккаунт</a>
-            </div>
+    if (this.state.user) {
+      return (
+        <div className="container">
+           <div className="form">
+              Привет, {this.state.user?.login}
+              <button className="button" name="exit" onClick={this.handleClick}>Выход</button>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="container">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <h1 className="form-title">Войти:</h1>
+          <div className="form-group">
+            <input className="form-input" name="login" placeholder="Логин" />    
+          </div>
+          <div className="form-group">
+            <input className="form-input" name="password" placeholder="Пароль" type="password" />
+          </div>
+          <button className="button" name="enter">Войти</button>
+          <div>
+            <a className="links" href="#">Забыли пароль?</a>
+          </div>
+          <div>
+            <a className="links" href="#">Создать аккаунт</a>
+          </div>
         </form>
       </div>
-      
-    )
-  }
-  
-}
-  
+    );
+  };
+};
 
-
-
-
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(< Form />);
-  
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(< Form />); 
